04ad59449f63405bef2e447c5783147f79f88903
Landon Neal <landonneal@Landons-MacBook-Air.local>
2021-11-26 10:27:42 -0500

    work on generation

--- src/main/scala/generator/MatrixGenerator.scala
+++ src/main/scala/generator/MatrixGenerator.scala
@@ -5,13 +5,15 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.random.RandomRDDs
 
 object MatrixGenerator {
+  private val MAX = 100
+
   def main(args: Array[String]) {
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
-    if (args.length != 4) {
-      logger.error("Usage:\ngenerator.MatrixGenerator <i> <j> <k> <output_dir>")
+    if (args.length != 5) {
+      logger.error("Usage:\ngenerator.MatrixGenerator <i> <j> <k> <fill> <output_dir>")
       System.exit(1)
     }
-    val conf = new SparkConf().setAppName("Matrix Generator")/*.setMaster("local[4]")*/
+    val conf = new SparkConf().setAppName("Matrix Generator").setMaster("local[4]")
     val sc = new SparkContext(conf)
 
     // matrixLeft has dimension i * j, matrixRight has dimension j * k
@@ -19,16 +21,31 @@ object MatrixGenerator {
     val j = args(1)
     val k = args(2)
 
-    val output = args(3)
+    // Used as probability that matrix value is non-zero
+    val fill_prob = args(3).toDouble
 
-    val leftMatrixRDD = RandomRDDs.uniformVectorRDD(sc, i.toInt, j.toInt)
+    val output = args(4)
 
-    leftMatrixRDD.saveAsTextFile(output + "/left")
+    val r = scala.util.Random
 
-    val rightMatrixRDD = RandomRDDs.uniformVectorRDD(sc, j.toInt, k.toInt)
+    val x = sc.range(0, i.toLong)
+    val y = sc.range(0, j.toLong)
+    val z = sc.range(0, k.toLong)
 
-    rightMatrixRDD.saveAsTextFile(output + "/right")
+    val left = x.cartesian(y).map {
+      case (x, y) => (x, y, if (r.nextDouble() < fill_prob) r.nextInt(MAX) else 0)
+    }.filter {
+      case (x, y, v) => v != 0
+    }
 
+    val right = y.cartesian(z).map {
+      case (x, y) => (x, y, if (r.nextDouble() < fill_prob) r.nextInt(MAX) else 0)
+    }.filter {
+      case (x, y, v) => v != 0
+    }
 
+    left.saveAsTextFile(output + "left")
+
+    right.saveAsTextFile(output + "right")
   }
 }
