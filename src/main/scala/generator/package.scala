1bd3fad5dc91a51632845e80244642629dfbf915
Zoheb Nawaz <zohebzameer@gmail.com>
2021-11-25 22:31:36 -0500

    Modified job name, deleted unnecessary comments

--- src/main/scala/generator/package.scala
+++ src/main/scala/generator/package.scala
@@ -11,14 +11,8 @@ object MatrixGenerator {
       logger.error("Usage:\nwc.WordCountMain <input dir> <output dir>")
       System.exit(1)
     }
-    val conf = new SparkConf().setAppName("Word Count")
+    val conf = new SparkConf().setAppName("Matrix Generator")
     val sc = new SparkContext(conf)
-
-    // Delete output directory, only to ease local development; will not work on AWS. ===========
-    //    val hadoopConf = new org.apache.hadoop.conf.Configuration
-    //    val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
-    //    try { hdfs.delete(new org.apache.hadoop.fs.Path(args(1)), true) } catch { case _: Throwable => {} }
-    // ================
 
     val textFile = sc.textFile(args(0))
     val counts = textFile.flatMap(line => line.split(" "))
