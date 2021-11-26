a71bd8984eeccd651d684bc76c3cc7c9bc985550
Zoheb Nawaz <zohebzameer@gmail.com>
2021-11-25 23:14:25 -0500

    Refactored scala object name, added random matrix generator code

---
+++ src/main/scala/generator/MatrixGenerator.scala
@@ -0,0 +1,34 @@
+package generator
+
+import org.apache.log4j.LogManager
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.random.RandomRDDs
+
+object MatrixGenerator {
+  def main(args: Array[String]) {
+    val logger: org.apache.log4j.Logger = LogManager.getRootLogger
+    if (args.length != 4) {
+      logger.error("Usage:\ngenerator.MatrixGenerator <i> <j> <k> <output_dir>")
+      System.exit(1)
+    }
+    val conf = new SparkConf().setAppName("Matrix Generator").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+
+    // matrixLeft has dimension i * j, matrixRight has dimension j * k
+    val i = args(0)
+    val j = args(1)
+    val k = args(2)
+
+    val output = args(3)
+
+    val leftMatrixRDD = RandomRDDs.uniformVectorRDD(sc, i.toInt, j.toInt)
+
+    leftMatrixRDD.saveAsTextFile(output + "/left")
+
+    val rightMatrixRDD = RandomRDDs.uniformVectorRDD(sc, j.toInt, k.toInt)
+
+    rightMatrixRDD.saveAsTextFile(output + "/right")
+
+
+  }
+}
