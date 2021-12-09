32cdcb855ad97775d90390f8db0e0df1293e9c82
Landon Neal <landonneal@Landons-MacBook-Air.local>
2021-12-09 14:27:21 -0500

    cleanup for aws

--- src/main/scala/product/SparseProduct.scala
+++ src/main/scala/product/SparseProduct.scala
@@ -4,44 +4,41 @@ import org.apache.log4j.LogManager
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 
-import scala.:+
-import scala.collection.mutable
+import java.nio.file.Paths
 
 object SparseProduct {
   private final val logger: org.apache.log4j.Logger = LogManager.getRootLogger
   final val P = 4 // # partitions, must be square
-  final val HP = new HashPartitioner(P)
+  final val HP = new HashPartitioner(P) // default partitioner
+  final val N = 5 // matrices are of dimension NxN
   type SparseRDD = RDD[(Int, Int, Long)] // (i, j, v)
 
   def main(args: Array[String]): Unit = {
-    if (args.length != 4) {
-      logger.error("Usage:\nproduct.SparseProduct <n> <a_dir> <b_dir> <output_dir>")
+    if (args.length != 2) {
+      logger.error("Usage:\nproduct.SparseProduct <input_dir> <output_dir>")
       System.exit(1)
     }
     val conf = new SparkConf().setAppName("Sparse Product").setMaster("local[4]")
     val sc = new SparkContext(conf)
 
-    // Both a and b matrices have dimension nxn
-    val n = args(0).toInt
-
-    val aDir = args(1)
-    val bDir = args(2)
-    val output = args(3)
+    val aDir = Paths.get(args(0), "a").toString
+    val bDir = Paths.get(args(0), "b").toString
+    val output = args(1)
 
     // Delete output directory, only to ease local development; will not work on AWS. ===========
-    val hadoopConf = new org.apache.hadoop.conf.Configuration
-    val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
-    try {
-      hdfs.delete(new org.apache.hadoop.fs.Path(output), true)
-    } catch {
-      case _: Throwable =>
-    }
+    //    val hadoopConf = new org.apache.hadoop.conf.Configuration
+    //    val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
+    //    try {
+    //      hdfs.delete(new org.apache.hadoop.fs.Path(output), true)
+    //    } catch {
+    //      case _: Throwable =>
+    //    }
     // ================
 
     val a = parseSparse(sc, aDir)
     val b = parseSparse(sc, bDir)
 
-    val product = VerticalHorizontal.sparseProduct(a, b, n, sc)
+    val product = VerticalHorizontal.sparseProduct(a, b, N, sc)
 
     product.saveAsTextFile(output)
   }
